7fb043e1a6904daeb541483df4303af381e2c84f
EmanuelHernandez <hernandezemanuelm@gmail.com>
2021-05-26 19:40:10 -0300

    Proceso-ControlAlquiler

---
+++ G11_TP_PAV/Formularios/ContratoAlquiler/Frm_ContratoAlquiler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using G11_TP_PAV.Negocio;
+using G11_TP_PAV.Negocio;
+
+namespace G11_TP_PAV
+{
+    public partial class Frm_ContratoAlquiler : Form
+    {
+        public Frm_ContratoAlquiler()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_ContratoAlquiler_Load(object sender, EventArgs e)
+        {
+            cmb_tipoMoneda.CargarCombo();
+            cmb_cliente.CargarCombo();
+            cmb_Propiedad.CargarCombo2();
+            cmb_escribano.CargarCombo2();
+
+        }
+        NE_ContratoAlquiler contrato = new NE_ContratoAlquiler();
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+
+            contrato.Pp_fechaInicio = DT_inicio.Value.Date.ToShortDateString();
+            contrato.Pp_duracionContrato = DT_duracion.Value.Date.ToShortDateString();
+            contrato.Pp_monto = txt_monto.Text;
+            contrato.Pp_porcentaje = cmb_tipoMoneda.SelectedValue.ToString();
+            contrato.Pp_propiedad = cmb_Propiedad.SelectedValue.ToString();
+            contrato.Pp_cliente = cmb_escribano.SelectedValue.ToString();
+            contrato.Pp_escribano = cmb_escribano.SelectedValue.ToString();
+
+            contrato.InsertarContratoAlquiler();
+            contrato.InsertarFactura();
+            MessageBox.Show("Se realizo el alta correctamente.");
+        }
+
+        public void DT_inicio1_ValueChanged(object sender, EventArgs e)
+        {
+            string fecha_inicio = DT_inicio.Value.Date.ToShortDateString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataGrid.Rows.Clear();
+            DataTable tabla = new DataTable();
+            tabla = contrato.RecuperarDatos();
+            CargarGrilla(tabla);
+        }
+        private void CargarGrilla(DataTable tabla)
+        {
+            if (tabla.Rows.Count == 0)
+            {
+                DataGrid.Rows.Clear();
+
+            }
+            else
+            {
+                for (int i = 0; i < tabla.Rows.Count; i++)
+                {
+                    DataGrid.Rows.Add();
+                    DataGrid.Rows[i].Cells[0].Value = tabla.Rows[i]["nro_factura"].ToString();
+                    DataGrid.Rows[i].Cells[1].Value = tabla.Rows[i]["propiedad"].ToString();
+                    DataGrid.Rows[i].Cells[2].Value = tabla.Rows[i]["documento"].ToString();
+                    DataGrid.Rows[i].Cells[3].Value = tabla.Rows[i]["id_tipo_moneda"].ToString();
+                    DataGrid.Rows[i].Cells[4].Value = tabla.Rows[i]["monto"].ToString();
+                    DataGrid.Rows[i].Cells[5].Value = tabla.Rows[i]["fecha_inicio"].ToString();
+
+
+
+                }
+            }
+        }
+    }
+}
---
+++ G11_TP_PAV/Negocio/NE_ContratoAlquiler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G11_TP_PAV.Clases;
+using System.Data;
+using System.Windows.Forms;
+
+namespace G11_TP_PAV.Negocio
+{
+    class NE_ContratoAlquiler
+    {
+        public string Pp_id_contrato { get; set; }
+        public string Pp_fechaInicio { get; set; }
+        public string Pp_duracionContrato { get; set; }
+        public string Pp_monto { get; set; }
+        public string Pp_porcentaje { get; set; }
+        public string Pp_propiedad { get; set; }
+        public string Pp_cliente { get; set; }
+        public string Pp_escribano { get; set; }
+
+        BE_Edificios _BD = new BE_Edificios();
+        DataTable tabla = new DataTable();
+
+
+
+        public DataTable RecuperarDatos()
+        {
+            string sql = "SELECT fact.nro_factura, cont.fecha_inicio,cont.monto * (0.01 *fact.comision) as monto,concat (moneda.nombre,'-',moneda.porcentaje_comision,'%') as id_tipo_moneda,CONCAT(prop.calle, prop.numero) as propiedad,cont.documento FROM facturas_comisiones fact join contratoAlquiler cont on cont.id_contratoALQ = fact.contrato_alquiler join propiedades prop on cont.designacion_catatral = prop.designacion_catastral join tipo_moneda moneda on cont.id_tipo_moneda = moneda.id_moneda";
+            return _BD.Ejecutar_Select(sql);
+
+        }
+        public void InsertarContratoAlquiler()
+        {
+            string sqlInsertar = "INSERT INTO contratoAlquiler (fecha_inicio,duracion_contrato,monto,id_tipo_moneda,designacion_catatral,documento) VALUES('" + Pp_fechaInicio + "' , '" + Pp_duracionContrato + "' , '" + Pp_monto + "' , '" + Pp_porcentaje + "', '" + Pp_propiedad + "', '" + Pp_cliente +  "' )";
+
+            _BD.Insertar(sqlInsertar);
+
+        }
+
+        public void InsertarFactura() {
+            string sqlSelect = "select * from contratoAlquiler where id_contratoALQ in(select MAX(id_contratoALQ) as ID from contratoAlquiler )";
+            tabla =_BD.Ejecutar_Select(sqlSelect);
+            Pp_id_contrato = tabla.Rows[0]["id_contratoALQ"].ToString();
+            string sqlInsertar = "INSERT INTO facturas_comisiones (comision,monto,fecha_pago,matricula_escribano,contrato_alquiler) VALUES('" + Pp_porcentaje + "' , '" + Pp_monto + "' , '" + Pp_fechaInicio + "' , '" + Pp_escribano + "', '" + Pp_id_contrato + "' )";
+            _BD.Insertar(sqlInsertar);
+        }
+    }
+}
